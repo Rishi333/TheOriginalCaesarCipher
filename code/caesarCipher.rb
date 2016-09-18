087d4a199fb5f275de8b455f01eb5ac68f7fd5bd
rbommu <rbommu@proofpoint.com>
2016-09-18 09:57:44 -0700

    punctuation does not shift

--- code/caesarCipher.rb
+++ code/caesarCipher.rb
@@ -1,20 +1,24 @@
 
-## Source code from :https://gist.github.com/matugm/db363c7131e6af27716c
-## modified in order to take first paramater as the encrypted ciphertext
+
 ALPHABET_SIZE = 26
 
 def caesar_cipher(string)
   shiftyArray = []
   charLine = string.chars.map(&:ord)
+  punctuation  = Array[ ' ', ',', '?', '.', '!', '\'' ]# ascii for basic punctuation
 
   shift = 1
   ALPHABET_SIZE.times do |shift|
     shiftyArray << charLine.map do |c|
-      ((c + shift) < 123 ? (c + shift) : (c + shift) - 26).chr
+      if punctuation.include?(c.chr) then # dont track punctuation
+        c.chr
+      else
+       ((c + shift) < 123 ? (c + shift) : (c + shift) - 26).chr
+      end
     end.join
   end
 
-  shiftyArray
+  return shiftyArray
 end
 
 puts caesar_cipher(ARGV[0])
